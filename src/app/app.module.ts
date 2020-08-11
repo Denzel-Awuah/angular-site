b7479c3aa9c19754950656f6d96be6209cd78ec4
Denzel Awuah <denzelawuah@Denzels-Macbook-Air.local>
2020-08-11 14:17:23 -0400

    added ni

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagefooterComponent } from './pagefooter/pagefooter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagefooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pagefooter/pagefooter.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagefooter',
+  templateUrl: './pagefooter.component.html'
+})
+export class PagefooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
